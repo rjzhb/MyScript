0d33f45170e0492f09b82b5e9b557778db97871d
rjzhb <986215159@qq.com>
2023-03-05 14:05:05 +0800

    'update‘

---
+++ GetFilesDelay.py
@@ -0,0 +1,20 @@
+import csv
+
+
+def merge_delays(i, j):
+    delays = []
+    for k in range(1, 100):
+        filename = "E:/Projects/DataSet/NetLatency-Data/Seattle/SeattleData_%d" % k
+        with open(filename, "r") as f:
+            lines = f.readlines()
+            delay = lines[i].split()[j]
+            delays.append(delay)
+    with open(
+        "E:/Projects/DataSet/CSV/delay/delay%d_%d.csv" % (i, j), "w", newline=""
+    ) as f:
+        writer = csv.writer(f)
+        writer.writerow(["delay"])
+        writer.writerows([[delay] for delay in delays])
+
+
+merge_delays(0, 2)
