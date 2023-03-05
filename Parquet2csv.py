108e2d8891e27fc3eac0c5019714b031f9e1934c
rjzhb <986215159@qq.com>
2023-03-05 13:39:44 +0800

    'commit'

---
+++ Parquet2csv.py
@@ -0,0 +1,6 @@
+import pyarrow.parquet as pq
+import pandas as pd
+
+parquet_file = pq.read_table('E:/Projects/DataSet/fhvhv_tripdata_2022-07.parquet')
+df = parquet_file.to_pandas()
+df.to_csv('E:/Projects/DataSet/CSV/fhvhv_tripdata_2022-07.csv', index=False)
