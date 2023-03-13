f551362246d2629bbf12e57f97351008d598c48a
rjzhb <986215159@qq.com>
2023-03-13 13:34:39 +0800

    update

--- MergeKeyAndDelay.py
+++ MergeKeyAndDelay.py
@@ -1,4 +1,7 @@
 import csv
+import csv
+import random
+import numpy as np
 
 # 合并key, te, ta三大属性
 
@@ -27,22 +30,42 @@ def read_delay_file(name):
     return data
 
 
-# 输出带有三列的csv文件，这三列属性分别是:  key , te,  ta( te + delay)
-keyData = read_key_file("E:/Projects/DataSet/DataSets/rovio/1000ms_1t.csv")
-delayData = read_delay_file("HighDelayData.csv")
+# 设置Zipf分布参数
+alpha = 1.5
+xmin = 1
+N = 10000000
 
-with open("E:/Projects/DataSet/DataSets/rovio/1000ms_1tHighDelayData.csv", "w", newline="") as csvfile:
+# 生成Zipf分布数据
+zipf_data = np.random.zipf(alpha, size=10000000)
+
+
+# 输出带有三列的csv文件，这三列属性分别是:  key , te,  ta( te + delay)
+keyData = read_key_file("E:/Projects/DataSet/DataSets/YSB/ad_events.csv")
+delayData = read_delay_file("MidDelayData.csv")
+
+with open(
+    "E:/Projects/DataSet/DataSets/YSB/ad_eventsMidDelayData.csv", "w", newline=""
+) as csvfile:
     writer = csv.writer(csvfile)
     writer.writerow(["key", "value", "eventTime", "arrivalTime"])
+    j = 0
     for i in range(len(keyData)):
-        writer.writerow(
-            [
-                keyData[i][0],
-                keyData[i][1],
-                keyData[i][2],
-                float(delayData[i]) * 1000 * 1000 + float(keyData[i][2]),
-            ]
-    
-        )
-        
-        
+        if i < len(delayData):
+            writer.writerow(
+                [
+                    keyData[i][0],
+                    keyData[i][1],
+                    keyData[i][2],
+                    float(delayData[i]) * 1000 * 1000 + float(keyData[i][2]),
+                ]
+            )
+        else:
+            writer.writerow(
+                [
+                    keyData[i][0],
+                    keyData[i][1],
+                    keyData[i][2],
+                    float(zipf_data[j]) * 1000 * 1000 + float(keyData[i][2]),
+                ]
+            )
+            j += 1
--- txt2CSV.py
+++ txt2CSV.py
@@ -1,17 +1,18 @@
 import csv
 
 # 读取txt文件
-with open("E:/Projects/DataSet/DataSets/rovio/1000ms_1t.txt", "r") as txt_file:
+with open("E:/Projects/DataSet/DataSets/YSB/ad_events.txt", "r") as txt_file:
     lines = txt_file.readlines()
+
 
 # 将数据写入csv文件
 with open(
-    "E:/Projects/DataSet/DataSets/rovio/1000ms_1t.csv", "w", newline=""
+    "E:/Projects/DataSet/DataSets/YSB/ad_events.csv", "w", newline=""
 ) as csv_file:
     writer = csv.writer(csv_file)
     writer.writerow(["key", "value", "eventTime"])
     for line in lines:
-        key = line.strip().split("|")[0]
-        value = line.strip().split("|")[2]
-        eventTime = line.strip().split("|")[3]
+        key = line.strip().split(",")[0]
+        value = 1
+        eventTime = line.strip().split(",")[1]
         writer.writerow([key, value, eventTime])
