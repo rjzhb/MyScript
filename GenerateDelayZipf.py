2c939e722786b349b9f84a8be9988e207da36a66
rjzhb <986215159@qq.com>
2023-03-14 13:39:02 +0800

    update

--- GenerateDelayZipf.py
+++ GenerateDelayZipf.py
@@ -1,6 +1,7 @@
 import csv
 import random
 import numpy as np
+import time
 
 # 设置Zipf分布参数
 alpha = 1.5
@@ -10,11 +11,20 @@ N = 100000
 # 生成Zipf分布数据
 zipf_data = np.random.zipf(alpha, size=N)
 
+# 获取当前时间戳
+current_timestamp = int(time.time() * 1000)
+
 # 将生成的网络延迟写入CSV文件
 with open('network_delay.csv', mode='w', newline='') as f:
     writer = csv.writer(f)
     writer.writerow(['delay'])
     for delay in zipf_data:
-        writer.writerow([delay])
-
+        # 计算生成的时间戳，确保在1秒以内
+        timestamp = current_timestamp + delay
+        if timestamp > current_timestamp + 1000:
+            timestamp = current_timestamp + 1000 - random.randint(1, 100)
+        
+        # 将延迟和时间戳写入CSV文件
+        writer.writerow([delay, timestamp])
+        
 print("网络延迟生成完毕并保存到文件：network_delay.csv")
--- MergeKeyAndDelay.py
+++ MergeKeyAndDelay.py
@@ -30,42 +30,24 @@ def read_delay_file(name):
     return data
 
 
-# 设置Zipf分布参数
-alpha = 1.5
-xmin = 1
-N = 10000000
-
-# 生成Zipf分布数据
-zipf_data = np.random.zipf(alpha, size=10000000)
-
-
 # 输出带有三列的csv文件，这三列属性分别是:  key , te,  ta( te + delay)
 keyData = read_key_file("E:/Projects/DataSet/DataSets/YSB/ad_events.csv")
-delayData = read_delay_file("MidDelayData.csv")
+delayData = read_delay_file("HighDelayData.csv")
 
 with open(
-    "E:/Projects/DataSet/DataSets/YSB/ad_eventsMidDelayData.csv", "w", newline=""
+    "E:/Projects/DataSet/DataSets/YSB/ad_eventsHighDelayData.csv", "w", newline=""
 ) as csvfile:
     writer = csv.writer(csvfile)
     writer.writerow(["key", "value", "eventTime", "arrivalTime"])
     j = 0
     for i in range(len(keyData)):
-        if i < len(delayData):
-            writer.writerow(
-                [
-                    keyData[i][0],
-                    keyData[i][1],
-                    keyData[i][2],
-                    float(delayData[i]) * 1000 * 1000 + float(keyData[i][2]),
-                ]
-            )
-        else:
-            writer.writerow(
-                [
-                    keyData[i][0],
-                    keyData[i][1],
-                    keyData[i][2],
-                    float(zipf_data[j]) * 1000 * 1000 + float(keyData[i][2]),
-                ]
-            )
-            j += 1
+        if j >= len(delayData):
+            j = 0
+        writer.writerow(
+            [
+                keyData[i][0],
+                keyData[i][1],
+                keyData[i][2],
+                float(delayData[j]) * 1000 * 1000 + float(keyData[i][2]),
+            ]
+        )
